0e88a4d50b0489785dc43fcb5c32b4065aedcc88
willow <shaneweth@gmail.com>
2019-03-07 17:08:14 -0500

    Added index.html with some structure. Also created game.js and style.css. Timer is working, but there is no response,  yet, when timer expires.

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+
+const start = document.getElementById("start");
+
+const quiz = document.getElementById("quiz");
+
+const question = document.getElementById("question");
+
+const optionA = document.getElementById("A");
+const optionB = document.getElementById("B");
+const optionC = document.getElementById("C");
+const optionD = document.getElementById("D");
+
+
+var timeLeft = 30;
+    var elem = document.getElementById('timerDemo');
+
+    var timerId = setInterval(countdown, 1000);
+
+    function countdown() {
+        if (timeLeft === 0) {
+            clearTimeout(timerId);
+            // doSomething will be the action the page takes after the clock has expired. 
+            doSomething();
+        } else {
+            elem.innerHTML = timeLeft + ' seconds remaining';
+            timeLeft--;
+        }
+    }
+
+// Variable for questions. THis may be replaced with an API or a JSON file. 
